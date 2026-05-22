8af1b1829459e2255c8c02fa7bc663cfadd6a2d6
Samrat Parajuli <code.samzt13@gmail.com>
2026-05-22 13:04:02 +0545

    Initial commit

---
+++ app/src/main/java/com/example/vsninventory/InventoryApp.kt
@@ -0,0 +1,10 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+package com.example.vsninventory
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InventoryApp(){
+
+}
